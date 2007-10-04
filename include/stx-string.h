59a0031d8f418f1cc4e14cf9a16b5ea11c9efded
Timo Bingmann <tbgit@panthema.net>
2007-10-04 08:40:07 +0000

    Creating automake compile environment with test program.

---
+++ include/stx-string.h
@@ -0,0 +1,17 @@
+// $Id$
+
+#ifndef _STX_STRING_H_ 
+#define _STX_STRING_H_
+
+#include <string>
+
+namespace stx {
+
+class string : public std::string
+{
+
+};
+
+} // namespace stx
+
+#endif // _STX_STRING_H_
---
+++ test.cc
@@ -0,0 +1,10 @@
+// $Id$
+
+#include <stx-string.h>
+
+int main()
+{
+    stx::string str;
+
+    return 0;
+}
