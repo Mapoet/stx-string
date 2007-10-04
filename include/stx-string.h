6332f3510de2303fc15af8313b8b91cd33fc734f
Timo Bingmann <tbgit@panthema.net>
2007-10-04 09:17:28 +0000

    Creating copies of all the constructors of std::string and adding simple trim functions.

--- include/stx-string.h
+++ include/stx-string.h
@@ -9,7 +9,148 @@ namespace stx {
 
 class string : public std::string
 {
+public:
 
+    /** Default constructor. Creates an empty string. */
+    string()
+	: std::string()
+    {
+    }
+
+    /** Copy-Constructor. Duplicates the enclosed STL string. */
+    string(const string &s)
+	: std::string(s)
+    {
+    }
+
+    /** Automatic Type-cast Constructor. Duplicates the given STL string. */
+    string(const std::string &s)
+	: std::string(s)
+    {
+    }
+
+    /** Repeating constructor. Creates a string which contains the charater ch
+     * repeated length times. */
+    string(size_type length, const char& ch)
+	: std::string(length, ch)
+    {
+    }
+
+    /** C-style string constructor. Fills the new string with the given C-style
+     * string. */
+    string(const char* str)
+	: std::string(str)
+    {
+    }
+
+    /** C-style string constructor with length. Fills the new string with data
+     * of length bytes. */
+    string(const char* str, size_type length)
+	: std::string(str, length)
+    {
+    }
+
+    /** Substring constructor. Creates a new string containing the substring
+     * specified by index and length. */
+    string(const string& str, size_type index, size_type length)
+	: std::string(str, index, length)
+    {
+    }
+    
+    /** Template-Iterator range constructor. Fills the new string with the
+     * character in the start-end range. */
+    template <typename input_iterator>
+    string(input_iterator start, input_iterator end)
+	: std::string(start, end)
+    {
+    }
+
+    /** Trims the contained string on the left and right. Removes all
+     * characters in the given drop array, which defaults to " ". Returns a
+     * copy of this string.
+     * @param drop	remove these characters
+     * @return		new trimmed string
+     */
+    string trim(const string& drop = " ") const
+    {
+	size_type pos1 = find_first_not_of(drop);
+	if (pos1 == npos) return string();
+
+	size_type pos2 = find_last_not_of(drop);
+	if (pos2 == npos) return string();
+
+	return substr(pos1 == npos ? 0 : pos1,
+		      pos2 == npos ? (size() - 1) : (pos2 - pos1 + 1));
+    }
+
+    /** Trims the contained string only on the left. Removes all characters in
+     * the given drop array, which defaults to " ". Returns a copy of this
+     * string.
+     * @param drop	remove these characters
+     * @return		new trimmed string
+     */
+    string trim_left(const string& drop = " ") const
+    {
+	size_type pos = find_first_not_of(drop);
+	if (pos == npos) return string();
+
+	return substr(pos, npos);
+    }
+
+    /** Trims the contained string only on the right. Removes all characters in
+     * the given drop array, which defaults to " ". Returns a copy of this
+     * string.
+     * @param drop	remove these characters
+     * @return		new trimmed string
+     */
+    string trim_right(const string& drop = " ") const
+    {
+	size_type pos = find_last_not_of(drop);
+	if (pos == npos) return string();
+
+	return substr(0, pos + 1);
+    }
+
+    /** Trims this string in-place on the left and right. Removes all
+     * characters in the given drop array, which defaults to " ".
+     * @param drop	remove these characters
+     * @return		reference to this string
+     */
+    string& trim_inplace(const string& drop = " ")
+    {
+	size_type pos = find_last_not_of(drop);
+	if (pos != npos) {
+	    erase(pos + 1);
+	    pos = find_first_not_of(drop);
+	    if (pos != npos) erase(0, pos);
+	}
+	else
+	    erase(begin(), end());
+
+	return *this;
+    }
+
+    /** Trims this string in-place only on the left. Removes all characters in
+     * the given drop array, which defaults to " ".
+     * @param drop	remove these characters
+     * @return		reference to this string
+     */
+    string& trim_left_inplace(const string &drop = " ")
+    {
+	erase(0, find_first_not_of(drop));
+	return *this;
+    }
+
+    /** Trims this string in-place only on the right. Removes all characters in
+     * the given drop array, which defaults to " ".
+     * @param drop	remove these characters
+     * @return		reference to this string
+     */
+    string& trim_right_inplace(const string &drop = " ")
+    {
+	erase(find_last_not_of(drop) + 1, npos);
+	return *this;
+    }
 };
 
 } // namespace stx
--- testsuite/SimpleTest.cc
+++ testsuite/SimpleTest.cc
@@ -7,13 +7,54 @@
 class SimpleTest : public CPPUNIT_NS::TestFixture
 {
     CPPUNIT_TEST_SUITE( SimpleTest );
-    CPPUNIT_TEST(test_prefix);
+    CPPUNIT_TEST(test_trim);
     CPPUNIT_TEST_SUITE_END();
 
 protected:
 
-    void test_prefix()
+    void test_trim()
     {
+	// string-copy functions
+	CPPUNIT_ASSERT( stx::string("  abc  ").trim_left() == "abc  " );
+	CPPUNIT_ASSERT( stx::string("abc  ").trim_left() == "abc  " );
+	CPPUNIT_ASSERT( stx::string("  ").trim_left() == "" );
+
+	CPPUNIT_ASSERT( stx::string("  abc  ").trim_right() == "  abc" );
+	CPPUNIT_ASSERT( stx::string("  abc").trim_right() == "  abc" );
+	CPPUNIT_ASSERT( stx::string("  ").trim_right() == "" );
+
+	CPPUNIT_ASSERT( stx::string("  abc  ").trim() == "abc" );
+	CPPUNIT_ASSERT( stx::string("abc  ").trim() == "abc" );
+	CPPUNIT_ASSERT( stx::string("  abc").trim() == "abc" );
+	CPPUNIT_ASSERT( stx::string("  ").trim() == "" );
+
+	// in-place functions
+	stx::string str1 = "  abc  ";
+	stx::string str2 = "abc  ";
+	stx::string str3 = "  ";
+
+	CPPUNIT_ASSERT( str1.trim_left_inplace() == "abc  " );
+	CPPUNIT_ASSERT( str2.trim_left_inplace() == "abc  " );
+	CPPUNIT_ASSERT( str3.trim_left_inplace() == "" );
+
+	str1 = "  abc  ";
+	str2 = "  abc";
+	str3 = "  ";
+
+
+	CPPUNIT_ASSERT( str1.trim_right_inplace() == "  abc" );
+	CPPUNIT_ASSERT( str2.trim_right_inplace() == "  abc" );
+	CPPUNIT_ASSERT( str3.trim_right_inplace() == "" );
+
+	str1 = "  abc  ";
+	str2 = "  abc";
+	str3 = "abc  ";
+	stx::string str4 = "  ";
+
+	CPPUNIT_ASSERT( str1.trim_inplace() == "abc" );
+	CPPUNIT_ASSERT( str2.trim_inplace() == "abc" );
+	CPPUNIT_ASSERT( str3.trim_inplace() == "abc" );
+	CPPUNIT_ASSERT( str4.trim_inplace() == "" );
     }
 };
 
